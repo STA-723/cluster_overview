478cd8911dc4de9c438f58a06bd28674c84c3cfb
Federico Ferrari <ff31@dcc-slogin-01.oit.duke.edu>
2020-02-24 20:51:34 -0500

    fix load R

--- example_Rscript.sh
+++ example_Rscript.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#SBATCH --partition=common,common-large,statdept-low
+#SBATCH --partition=common,statdept-low
 #SBATCH --account=statdept
 #SBATCH -c1
 
+module load R/3.6.0
 Export R_LIBS_USER = ~/R/x86_64-pc-linux-gnu-library/3.6
 Rscript example.R
