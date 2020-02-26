0b26c6d9f124e75b1226e01ed350c756c1cfab2d
Federico Ferrari <ff31@duke.edu>
2020-02-26 12:02:40 -0500

    fix example array

--- example_array.sh
+++ example_array.sh
@@ -2,12 +2,11 @@
 #SBATCH -J example2
 #SBATCH --partition=statdept,herringlab-low,volfovskylab-low,common
 #SBATCH --account=sta723-s20
-#SBATCH --output=results/%x_%a.out
-#SBATCH --error=results/%x_%a.err
+#SBATCH --output=results/%x.out
 #SBATCH --array=1-5
 #SBATCH --cpus-per-task 2 
 #SBATCH --mem=4G
 
-Export R_LIBS_USER = ~/R/x86_64-pc-linux-gnu-library/3.6
+export R_LIBS_USER=~/R/x86_64-pc-linux-gnu-library/3.6
 module load R
 Rscript array.R
